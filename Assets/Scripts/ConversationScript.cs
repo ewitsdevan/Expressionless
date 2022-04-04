340a72f4ef33cf6166891633b17056a2b8110010
Devan Botheras <53108221+ewitsdevan@users.noreply.github.com>
2022-04-04 20:23:54 +1000

    FPS Controller & Test Environment

---
+++ Assets/Scripts/ConversationScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConversationScript : MonoBehaviour
+{
+    public bool canInteract;
+
+    public string question;
+    public string option1;
+    public string option2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Conversation()
+    {
+
+    }
+}
